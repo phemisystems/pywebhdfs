0fa1761e2550b2869e3aa0c8cac194cf52fb09c3
ata6 <ata6@sfu.ca>
2016-02-21 23:17:39 -0800

    WIP: cleaned up kerberos utility class

--- pywebhdfs/kerberos_utils.py
+++ pywebhdfs/kerberos_utils.py
@@ -1,34 +1,53 @@
 import kerberos
-from subprocess import Popen, PIPE
+import os.path
+import subprocess
 
 
-class KerberosHelper(object):
-    def __init__(self, krb_server, credentials, keytab):
-        self.krb_server = krb_server
-        self.credentials = credentials
-        self.keytab = keytab
+class KerberosContextManager(object):
+    def __init__(self, krb_conn_settings, use_keytab=True):
+        for param in ['principal', 'realm', 'server']:
+            if param not in krb_conn_settings:
+                raise ValueError('Missing parameter {0}'.format(param))
 
-        self.refresh_kerberos_cache()
+        if use_keytab:
+            if 'keytab' not in krb_conn_settings:
+                raise ValueError('Failed to specify path to keytab')
+            keytab = krb_conn_settings['keytab']
+            if not os.path.isfile(keytab):
+                raise ValueError('Keytab does not exist')
+        else:
+            if 'passwd' not in krb_conn_settings:
+                raise ValueError('Password is required if not using keytab')
 
+        self.krb_conn_settings = krb_conn_settings
+        self.use_keytab = use_keytab
+
+    def refresh_kerberos_tgt(self, aux_args=None):
+        kinit_cmd = ['kinit']
+        if aux_args:
+            kinit_cmd.extend(aux_args)
+
+        if self.use_keytab:
+            keytab = self.krb_conn_settings['keytab']
+            kinit_cmd.extend(['-k', '-t', keytab])
+
+        credentials = '{0}@{1}'.format(self.krb_conn_settings['principal'],
+                                       self.krb_conn_settings['realm'])
+
+        try:
+            err_msg = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+            if err_msg:
+                return False, err_msg
+        except subprocess.CalledProcessError as err:
+            return False, err.output
+
+        return True, ""
 
     def acquire_kerberos_context(self):
-        kerberos_principal = self.credentials["principal"]
-        kerberos_realm = self.credentials["realm"]
-        keytab_filepath = self.keytab
+        krb_server = self.krb_conn_settings['server']
+        _, krb_context = kerberos.authGSSClientInit(krb_server)
 
-        _, krb_context = kerberos.authGSSClientInit(self.krb_server)
         kerberos.authGSSClientStep(krb_context, "")
         ticket = kerberos.authGSSClientResponse(krb_context)
 
         return ticket
-
-
-    def refresh_kerberos_cache(self):
-        # FIXME Temporary hack for debugging. We need to be sure that we have
-        # a valid ticket before trying to authenticate against Kerberos.
-        kinit = Popen(["kinit", "-k", "-t", self.keytab,
-                "%s@%s" % (self.credentials['principal'], self.credentials['realm'])],
-                stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        kinit.wait()
-        if kinit.stderr.readlines():
-            return None
