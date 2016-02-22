3dfea41d193c3ccddaff4370447b25af49839300
Andrei Titerlea <atiterlea@phemi.com>
2016-02-22 10:53:13 -0800

    WIP: More updates to kerberos_utils.py

--- pywebhdfs/kerberos_utils.py
+++ pywebhdfs/kerberos_utils.py
@@ -23,28 +23,24 @@ class KerberosContextManager(object):
         self.use_keytab = use_keytab
 
     def refresh_kerberos_tgt(self, aux_args=None):
-        kinit_cmd = ['kinit']
-        if aux_args:
-            kinit_cmd.extend(aux_args)
-
-        if self.use_keytab:
-            keytab = self.krb_conn_settings['keytab']
-            kinit_cmd.extend(['-k', '-t', keytab])
-
         credentials = '{0}@{1}'.format(self.krb_conn_settings['principal'],
                                        self.krb_conn_settings['realm'])
 
+        kinit_cmd = ['kinit', credentials]
+        if aux_args:
+            kinit_cmd.extend(aux_args)
+
         try:
             if self.use_keytab:
-                err_msg = subprocess.check_output(*kinit_cmd,
-                                                  stderr=subprocess.STDOUT)
+                keytab = self.krb_conn_settings['keytab']
+                kinit_cmd.extend(['-k', '-t', keytab])
+
+                err_msg = subprocess.check_output(*kinit_cmd, stderr=subprocess.STDOUT)
                 if err_msg:
                     return False, err_msg
             else:
-                kinit = subprocess.Popen(*kinit_cmd,
-                                         stdin=PIPE, stdout=PIPE, stderr=PIPE)
-                kinit.stdin.write('{0}\n'.
-                                  format(self.krb_conn_settings['passwd']))
+                kinit = subprocess.Popen(*kinit_cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+                kinit.stdin.write('{0}\n'.format(self.krb_conn_settings['passwd']))
                 kinit.wait()
         except subprocess.CalledProcessError as err:
             return False, err.output
