9582c48dfb01ff180f6c81df2bb3002131072920
Andrei Titerlea <atiterlea@phemi.com>
2016-02-24 15:17:25 -0800

    touch ups

--- pywebhdfs/kerberos_utils.py
+++ pywebhdfs/kerberos_utils.py
@@ -26,21 +26,20 @@ class KerberosContextManager(object):
         self.krb_conn_settings = krb_conn_settings
         self.using_keytab = using_keytab
 
-    def _lookup_krbtgt_times(self, context, principal, ccache):
-        credential_times = namedtuple('CredentialTime',
-                                      'auth_time valid_starting expiry_date renew_until')
-
+    @staticmethod
+    def _lookup_krbtgt_times(context, principal, ccache):
+        credential_times = namedtuple('CredentialTime', 'auth_time valid_starting expiry_date renew_until')
         ticket_granting_ticket = 'krbtgt/{0}@{0}'.format(principal.realm)
-        tgt_principal = krbV.Principal(name=ticket_granting_ticket, context=context)
-        credentials = (principal, tgt_principal, (0, None), (0, 0, 0, 0), None, None, None, None,
-                       None, None)
+
+        tgt_principal = krbV.Principal(ticket_granting_ticket, context)
+
+        credentials = (principal, tgt_principal, (0, None), (0, 0, 0, 0), None, None, None, None, None, None)
         result = ccache.get_credentials(credentials, krbV.KRB5_GC_CACHED, 0)
 
         return credential_times._make(datetime.fromtimestamp(t) for t in result[3])
 
     def _build_kinit_cmd(self, aux_args):
-        credentials = '{0}@{1}'.format(self.krb_conn_settings['principal'],
-                                       self.krb_conn_settings['realm'])
+        credentials = '{0}@{1}'.format(self.krb_conn_settings['principal'], self.krb_conn_settings['realm'])
 
         kinit_cmd = ['kinit', credentials]
         if aux_args:
@@ -54,13 +53,10 @@ class KerberosContextManager(object):
         if self.using_keytab:
             krb_context = krbV.default_context()
             krb_ccache = krb_context.default_ccache()
-            krb_principal = krbV.Principal(name=self.krb_conn_settings['principal'],
-                                           context=krb_context)
+            krb_principal = krbV.Principal(self.krb_conn_settings['principal'], krb_context)
 
             try:
-                credential_times = self._lookup_krbtgt_times(context=krb_context,
-                                                             principal=krb_principal,
-                                                             ccache=krb_ccache)
+                credential_times = self._lookup_krbtgt_times(krb_context, krb_principal, krb_ccache)
                 current_date = datetime.now()
                 time_remaining = credential_times.expiry_date - current_date
                 if time_remaining < timedelta(minutes=5):
@@ -91,7 +87,7 @@ class KerberosContextManager(object):
             except subprocess.CalledProcessError as badcall:
                 return False, badcall.output
             except OSError as oserr:
-                return False, oserr.strerr
+                return False, oserr.strerror
 
         return True, ""
 
@@ -99,8 +95,7 @@ class KerberosContextManager(object):
         self.refresh_kerberos_ccache()
 
         krb_server = 'HTTP@{0}'.format(self.krb_conn_settings['server'])
-        _, krb_context = kerberos.authGSSClientInit(service=krb_server,
-                                                    principal=self.krb_conn_settings['principal'])
+        _, krb_context = kerberos.authGSSClientInit(service=krb_server, principal=self.krb_conn_settings['principal'])
 
         kerberos.authGSSClientStep(krb_context, '')
         ticket = kerberos.authGSSClientResponse(krb_context)
